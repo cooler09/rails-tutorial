a6935da521045ded8ae125392f917aa8ff6982d7
Zachary Lockhart <zacharyjordan82@gmail.com>
2018-06-16 14:15:19 -0400

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,9 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+    def edit
+        @article = Article.find(params[:id])
+    end
     def create
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
@@ -26,6 +29,12 @@ class ArticlesController < ApplicationController
         else
           render 'edit'
         end
+    end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+    
+        redirect_to articles_path
     end
     private
     def article_params
